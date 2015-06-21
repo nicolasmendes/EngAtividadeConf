b30ec447eaa5b566c3e6e153eb52cc800f22b93d
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 22:14:23 -0300

    versao 1.2

--- src/engatividadeconf/A.java
+++ src/engatividadeconf/A.java
@@ -15,6 +15,16 @@ public class A {
     
     private String A3;
 
+    private String Aprinc4;
+
+    public String getAprinc4() {
+        return Aprinc4;
+    }
+
+    public void setAprinc4(String Aprinc4) {
+        this.Aprinc4 = Aprinc4;
+    }
+
     private String Aramo4;
 
     public String getAramo4() {
